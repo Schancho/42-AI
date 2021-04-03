567fd9b82b7817c284a5701c51060332633b2789
Schancho <sliman.chan@gmail.com>
2021-04-03 17:15:16 +0100

    42-AI BootCamp

--- boot_camp_python/module00/ex06/recipe.py
+++ boot_camp_python/module00/ex06/recipe.py
@@ -6,7 +6,3 @@ def     add_recipe(recipe_name, ingredients, meal_type, preparation_time):
 
     recipe = {'ingredients' : ingredients, 'meal type' : meal_type, 'preparation type' : preparation_time}
     cookbook[recipe_name] = recipe
-
-add_recipe("Sandwich", "", "tawajba", 10)
-add_recipe()
-print(cookbook)
